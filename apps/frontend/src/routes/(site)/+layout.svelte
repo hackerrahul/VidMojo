eb9f67ee411a21c639ff487b44dcf39ac6e416e8
HackerRahul <rahul13gangotri@gmail.com>
2024-06-26 19:32:28 +0530

    update

---
+++ apps/frontend/src/routes/(site)/+layout.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+    import { site_name, site_logo_dark } from "$lib/config";
+
+</script>
+<div class="relative z-20 pt-[60px] md:h-screen md:max-h-[950px] md:pt-0">
+
+    <!-- Header -->
+    <div class="max-w-5xl mx-auto sticky top-0">
+
+        
+        <div class="p-4">
+            <div class=" flex items-center justify-between py-2 px-5 rounded-2xl border dark:border-muted/80 backdrop-blur-3xl shadow-md">
+                <div class="inline-flex items-center gap-2 select-none">
+                    <img class="size-5" src={site_logo_dark} alt={site_name}>
+                    <p class="text-sm font-inter-500">{site_name}</p>
+                </div>
+                <div class="text-sm space-x-2 flex items-center">
+
+                    <div class="mr-2 flex items-center gap-4">
+                        <a href="/" class="font-inter-500 text-sm">Pricing</a>
+                        <a href="/" class="font-inter-500 text-sm">Features</a>
+                    </div>
+
+                    <Button href="/auth/login" variant="outline" size="sm">Login</Button>
+                    <Button href="/auth/login" variant="default" size="sm">Signup</Button>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <!-- Header -->
+
+    <img alt="Light ray background" fetchpriority="high" width="1000" height="1000" decoding="async" data-nimg="1" class="pointer-events-none absolute -top-20 left-0 right-0 z-0 mx-auto hidden h-full w-full select-none md:block" style="color: transparent;" srcset="/_next/image?url=%2Fstatic%2Flanding-page%2Fbghero.png&amp;w=1080&amp;q=75 1x, https://resend.com/_next/image?url=%2Fstatic%2Flanding-page%2Fbghero.png&w=2048&q=75 2x" src="https://resend.com/_next/image?url=%2Fstatic%2Flanding-page%2Fbghero.png&w=2048&q=75">
+
+
+<slot />
+
+</div>
--- apps/frontend/src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    import { Button } from "$lib/components/ui/button";
-</script>
-
-<Button>Click me</Button>
